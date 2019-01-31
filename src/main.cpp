029928b102edb1a1d304fe5ba4abe1a2cadf9da0
Arie Haryana <arie.haryana@gmail.com>
2019-02-01 01:44:41 +0700

    fixed: is mug available and disrupt motor servo

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@
 #include "MFRC522.h"
 #include <ArduinoJson.h>
 
-StaticJsonBuffer<200> jsonBuffer;
-
 #define RST_PIN D1
 #define SS_PIN D2
 
@@ -17,19 +15,25 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 Servo servo;
 
 // INIT WEMOS ESP - WIFI
-const char* ssid     = "Pixel_1687";
-const char* password = "04nov1994";
+const char* ssid     = "ARIF";
+const char* password = "";
 
 // INIT HTTP ACCESS
 HTTPClient http;
 const char *HTTP_= "http://";
-const char *SERVER= "192.168.43.117:3000";
-const char *API_GET_DATA  = "/users/";
-const char *API_POST_DATA  = "/drinks";
+const char *SERVER= "192.168.3.89";
+const char *API_GET_DATA  = "/api/users";
+const char *API_POST_DATA  = "/api/users/drinks/insert";
 
 // INIT TIMER
-int volume = 10000; // 250 ML
+int pull_time = 10000; // 250 ML
 unsigned long time_now = 0;
+
+int pull = 0;
+int user_id = 0;
+
+bool is_servo_trigerred = false;
+bool is_mug_available = false;
 
 
 // METHOD FIND USER
@@ -37,68 +41,92 @@ int find_user_id(String rfid){
   String _tempURI = HTTP_; 
 	_tempURI += SERVER;
 	_tempURI += API_GET_DATA;
+	_tempURI += "?rfid=";
 	_tempURI += rfid;
 	http.begin(_tempURI);
 
-	http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+	http.addHeader("Content-Type", "application/x-www-form-urlencoded"); 
 	int httpCode = http.GET();
-	int user_id;
-
-	if (httpCode == 200){
+    
+    if (httpCode == 200){
 			String payload = http.getString();
+
+			StaticJsonBuffer<300> jsonBuffer;
 			JsonObject& users = jsonBuffer.parseObject(payload);
-			user_id = users['data']['user_id'];
+			//Serial.println(payload);
+			user_id = users["data"]["id"];
+			Serial.print("User ID: ");
+			Serial.println(user_id);
+
+			pull = 1;
+
+			return true;
+      http.end();
 
 	}else{
-		return false;
-		Serial.println("Error on HTTP request");
-	}
-
-	http.end();
-
-	return user_id;
+			Serial.println("User not found!");
+			pull = 0;
+			return false;
+			http.end();
+  }
 }
 
 // METHOD SEND DATA
 int insert_data(int user_id, int val){
+	int vol = val * 25 / 1000;
+	String v = String(vol);
+	String u = String(user_id);
+  String postData = "user_id=" + u + "&milis=" + v;
+	
+	Serial.println(postData);
+
   String _tempURI = HTTP_;
   _tempURI += SERVER;
-  _tempURI += API_POST_DATA;
+  _tempURI += API_POST_DATA;	
   http.begin(_tempURI);
-
-	int vol = val * 25 / 1000;
-
-  String _postData = "user_id=" + user_id;
-	 _postData += "&value=" + vol;
-
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  int httpCode = http.POST(_postData);
-  http.end();
 
-  Serial.println(httpCode);
- 
+	
+  int httpCode = http.POST(postData);
+
   return httpCode;
+
+	http.end();
+
 }
 
 bool servo_on(){
 	servo.write(90);
+
+	is_servo_trigerred = true;
+	return true;
+}
+
+bool reset_variable(){
+	user_id = 0;
+	pull = 0;
+	time_now = 0;
+	is_servo_trigerred = false;
+
 	return true;
 }
 
 bool servo_off(){
 	servo.write(0);
+	reset_variable();
+	delay(5000);
 	return true;
 }
 
-bool is_mug_available(){
-	 int data = analogRead(A0);
-
-	 if(data < 90){
-		 return true;
+bool set_mug(){
+	 int infra = analogRead(A0);
+	 if(infra < 500){
+		 is_mug_available = true;
 	 }else{
-		 return false;
+		 //Serial.println("Mug tidak ditemukan");
+		 is_mug_available = false;
 	 }
-	 
+	 return true;
 }
 
 // SETUP
@@ -143,51 +171,45 @@ String generate_rfid(byte *buffer, byte bufferSize) {
 
 void loop(){
 	if ((WiFi.status() == WL_CONNECTED)){
+
+		set_mug();
 			
-			if ( ! mfrc522.PICC_IsNewCardPresent()) {
-					delay(50);
-					return;
+		String rfid;
+		
+		if(is_servo_trigerred){
+			if((millis() > time_now + pull_time) || !is_mug_available){
+				int vol = millis() - time_now;
+				insert_data(user_id, vol);
+				servo_off(); 
 			}
+		}
 			
-			if ( ! mfrc522.PICC_ReadCardSerial()) {
-					delay(50);
-					return;
-			}
-
-			if( ! is_mug_available()){
-				return;
-			}
-
-			String rfid = generate_rfid(mfrc522.uid.uidByte, mfrc522.uid.size);
-			int user_id = find_user_id(rfid);
-
-			if(user_id){
-					if(millis() > time_now + volume){
-
-						time_now = millis();
-						if(is_mug_available()){
-							servo_on();
-
-							if(volume > time_now){
-								servo_off();
-								insert_data(user_id, volume);
-								time_now = 0;
-
-								return;
-							}
-
-						}else{
-							servo_off();
-							insert_data(user_id, time_now);
-							time_now = 0;
-
+			switch(pull) {
+				case 0:
+					if ( ! mfrc522.PICC_IsNewCardPresent()) {
+							delay(50);
 							return;
-						}
-						
 					}
 					
-			}
-  
+					if ( ! mfrc522.PICC_ReadCardSerial()) {
+							delay(50);
+							return;
+					}
+
+					rfid = generate_rfid(mfrc522.uid.uidByte, mfrc522.uid.size);
+					find_user_id(rfid);
+					break;
+
+				case 1:
+					if(millis() > time_now + pull_time){
+						time_now = millis();
+
+						if(is_mug_available){
+							servo_on();
+						}
+					}
+					break;
+			}  
 	}
 
 }
